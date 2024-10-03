27d59fa89048b17c9d1aeca7a689d58d3a5bf665
Ankit Sharma <ankitsharma@users.noreply.github.com>
2024-10-03 11:14:53 +0100

    Update app.py
    Updated the SQS configuration and Code.

--- app.py
+++ app.py
@@ -3,9 +3,20 @@ import xmltodict, json
 import xml.etree.ElementTree as ET
 from lxml import etree
 import logging
+import boto3
 
 app = Flask(__name__)
+
 logger = logging.getLogger()
+
+# Initialize the SQS client
+
+sqs = boto3.client('sqs', region_name='<QUEUE_REGION_NAME>', 
+                   aws_access_key_id='<ACCESS_KYE>',
+                   aws_secret_access_key='<SECRET_ACCESS_KEY>')
+
+# Replace 'your-queue-url' with the actual URL of your SQS queue
+SQS_QUEUE_URL = '<YOUR_QUEUE_URL>'
 
 # The same Lambda function logic, but adapted to work with Flask
 @app.route('/transform', methods=['POST'])
@@ -16,7 +27,6 @@ def lambda_handler():
         
         if not xml_body:
             return json.dumps({"error": "No XML body provided"}), 400
-        #dom = xml.etree.fromstring(xml_body)
         dom = etree.fromstring(xml_body)
         xsl = etree.parse('transform.xsl')
         transform = etree.XSLT(xsl)
@@ -27,7 +37,24 @@ def lambda_handler():
         json_data = json.dumps(cleaned_json)
         
     #    return jsonify(cleaned_json), 200
-        return (json_data), 200
+    #    return (json_data), 200
+
+    # Publish the message to SQS
+        try:
+            response = sqs.send_message(
+                QueueUrl=SQS_QUEUE_URL,
+               MessageBody=json_data
+            )
+           # Log success for monitoring
+            print(f"Message sent to SQS: {response['MessageId']}")
+        except Exception as e:
+            raise RuntimeError(f"Failed to send message to SQS: {str(e)}")
+
+        # Return a success response
+        return {
+                'message': 'Data successfully processed and sent to SQS',
+                'sqs_message_id': response['MessageId']
+        }
 
     except Exception as e:
         # General error handling for unforeseen issues
